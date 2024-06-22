c605d472a47f8cfa76acad8b08610eda09bfcff6
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-06-22 12:46:05 +0800

    New characters and useEffect function

--- README.md
+++ README.md
@@ -3,15 +3,4 @@
 Project implemented to understand the useEffect hook in React applications.
 
 # Credits 
-
-Image by <a href="https://pixabay.com/users/wvlfy-32345648/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7676809">Wvlfy</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7676809">Pixabay</a>
-
-Image by <a href="https://pixabay.com/users/htchnm-14967706/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5814250">Htc Erl</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5814250">Pixabay</a>
-
-Image by <a href="https://pixabay.com/users/riki32-8327183/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7203817">Riki32</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7203817">Pixabay</a>
-
-<a href="https://wallpapers.com/wallpapers/aesthetic-sasuke-in-city-4k4fio45zm7k8epm"/>Sasuke picture</a>
-
-<a href="https://wallpapers.com/wallpapers/gon-freecss-black-powers-6q39ftg3lm6jmn72.html"/>Gon picture</a>
-
-<a href="https://wallpapers.com/wallpapers/neji-hyuga-otwfra4pv99zcckm.html"/>Neji Hyuga</a>
+The pictures were borrowed from <a href="https://jujutsu-kaisen.fandom.com/wiki/Jujutsu_Kaisen_Wiki">Jujutsu Kaisen Fandom Wiki Page</a>
--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,39 @@ import geto from "./assets/Suguru Geto.jpg";
 import kugisaki from "./assets/Nobara Kugisaki.jpg";
 import yuji from "./assets/Itadori Yuji.jpg";
 import sukuna from "./assets/Ryomen Sukuna.jpg";
+import choso from "./assets/Choso.jpg";
+import kenjaku from "./assets/Kenjaku.jpg";
+import mahito from "./assets/Mahito.jpg";
+import manami from "./assets/Manami Suda.jpg"
+import uraume from "./assets/Uraume.jpg";
+import yuki from "./assets/Yuki Tsukumo.jpg";
+import nanami from "./assets/Kento Nanami.jpg";
+import panda from "./assets/Panda.jpg";
+import toge from "./assets/Toge Inumaki.jpg";
 
-const characters = [
-  { name:  "Gojo Satoru", url: gojo },
-  { name:  "Aoi Todo", url: aoi  },
-  { name:  "Itadori Yuji", url: yuji },
-  { name:  "Ryomen Sukuna", url: sukuna },
-  { name:  "Megumi Fushiguro", url: megumi },
-  { name:  "Toji Fushigiro", url: toji },
-  { name:  "Yuta Okkotsu", url: yuta },
-  { name:  "Maki Zenin", url: maki },
-  { name:  "Mai Zenin", url: mai },
-  { name:  "Suguru Geto", url: geto },
-  { name:  "Nobara Kugisaki", url: kugisaki },
+const getName = (c) => {
+  let a = c.split("/");
+  a = a[a.length - 1].split(".")[0].split("%20").join(" ");
+  return a;
+}; 
+
+const characters = [ 
+  nanami, panda, toge, gojo, 
+  aoi, yuji, sukuna, megumi, 
+  toji, yuta, maki, mai, geto, 
+  kugisaki, choso, kenjaku, 
+  mahito, manami, uraume, yuki,
 ];
 
-// We will change the game display such that 
-// One card will be shown to the player at a time and the player 
-// will determine whether he or she has already seen the picture
-// Until all the cards have been seen, the game will continue asking the player
-
-function Card({ card, handleUpdateScore }) {
+function Card({ character, handleUpdateScore }) {
+  const name = getName(character);
   return (
     <div
       className="card-wrapper"
       onClick={handleUpdateScore}
     >
-      <img id={card.name} src={card.url} alt={card.name} />
-      <div className="card-name">{card.name}</div>
+      <img id={name} src={character} alt={name} />
+      <div className="card-name">{name}</div>
     </div>
   );
 }
@@ -49,7 +54,7 @@ function CardBoard({ list, score, bestScore, handleUpdateScore }) {
       <div className="header">
         <div id="game-title">Memory Card</div>
         <div id="game-description">
-          The game is simple, try not to click the same card twice to get the
+          The game is simple, avoid clicking on the same card twice to 
           increase your score.<br/> Let's see how good is your memory!
         </div>
 
@@ -62,12 +67,13 @@ function CardBoard({ list, score, bestScore, handleUpdateScore }) {
         {
                     list.map((e) => (
                         <Card 
-                            key={e.name} 
-                            card={e} 
+                            key={getName(e)} 
+                            character={e} 
                             handleUpdateScore={handleUpdateScore} 
                         />
         ))}
       </div>
+      <a href="https://blackbird410.github.io/blackbird410/" target="_blank">Copyright © Neil Taison Rigaud</a>
     </div>
   );
 }
@@ -76,12 +82,27 @@ function App() {
   const [cards, setCards] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [cardList, setCardList] = useState(characters);
+
+
+  // Shuffle the cards and rebuild the board 
+  useEffect(() => {
+    let temp = [...characters];
+    let newList = [];
+
+    while(temp.length) {
+      const pickIndex = Math.floor(Math.random() * temp.length);
+      
+      newList = [...newList, temp.splice(pickIndex, 1)[0]];
+    }
+    setCardList(newList);
+
+  }, [score]);
 
   const handleUpdateScore = (e) => {
     const cardName = e.target.id;
-    console.log(cardName, cards);
+    setBestScore(score > bestScore ? score : bestScore);
 
-    // If the card was not already seen score is incremented, otherwise becomes 0
     if (cards.includes(cardName)) {
       // You lost => Reset game
       setScore(0);
@@ -89,14 +110,13 @@ function App() {
     } else {
       setScore(score + 1);
       setCards([...cards, cardName]);
-      setBestScore(score > bestScore ? score : bestScore);
     }
   };
 
   return (
     <>
       <CardBoard
-        list={characters}
+        list={cardList}
         score={score}
         bestScore={bestScore}
         handleUpdateScore={handleUpdateScore}
